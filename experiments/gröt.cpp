516c1f93b6530dbfae9aec233c33b94a23901eed
Gustav Kalander <gustav.kalander@hotmail.se>
2021-01-02 17:22:28 +0100

    [experiments gröt] removed debug code

--- experiments/gröt.cpp
+++ experiments/gröt.cpp
@@ -172,40 +172,6 @@ int solve_binsearch_math(){
     return ans;
 }
 
-int gettime(int p, int m){
-    int t = 0;
-    queue<int> k;
-    for(int j = 0; j < p; j++){
-        t += 2;
-        k.push(t+m);
-    }
-    int cnt = 0;
-    while(sz(k) && k.front() <= t){
-        t += 1;
-        k.pop();
-        k.push(t+m);
-        cnt++;
-        if(cnt > N+10){
-            t = inf;
-            break;
-        }
-    }
-    return t;
-}
-
-int gettime2(int p, int m){
-    int t;
-    if(p > m) t = inf;
-    else{
-        t = 2*p;
-        if(p > (m+1)/2){
-            t += 2*(p-(m+1)/2);
-            if(m%2 == 0) t--;
-        }
-    }
-    return t;
-}
-
 void gen_random(int max_n, int max_m, int max_o){
     N = rand()%max_n + 1;
     M = rand()%max_m + 1;
@@ -217,12 +183,6 @@ void gen_random(int max_n, int max_m, int max_o){
 
 int main(){
     srand(time(0));
-/*
-    int m = 10;
-    for(int p = 0; p < 15; p++){
-        cout << p << " " << gettime(p, m) << " " << gettime2(p, m) << endl;
-    }
-    return 0;*/
 
     int t = 100000;
     for(int i = 0; i < t; i++){
