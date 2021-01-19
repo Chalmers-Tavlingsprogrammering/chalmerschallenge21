f304fba78bb1cdf78102e99e7e3b3b400b9825b7
Juglone <juliaam@protonmail.com>
2021-01-19 15:52:28 +0100

    [kcud] created validator, small change in problem statement & accepted submission

---
+++ Uppgifter/donaldduck/input_format_validators/validator/validator.cpp
@@ -0,0 +1,13 @@
+#include "validator.h"
+
+void run() {
+    int maxN = 1000000;
+    int maxM = 1000000;
+
+    int N = Int(0, maxN);
+    Space();
+    int M = Int(0, maxM);
+    Endl();
+
+    assert(N != M);
+}
