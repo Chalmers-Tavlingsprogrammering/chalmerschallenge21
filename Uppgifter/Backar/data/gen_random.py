6ebd73a2df6e6f36d4aefb4be5b6305e41a9fec6
loovjo <jonathan.loov@gmail.com>
2021-01-20 15:52:30 +0100

    Implement good random generator

--- Uppgifter/Backar/data/gen_random.py
+++ Uppgifter/Backar/data/gen_random.py
@@ -14,9 +14,54 @@ random.seed(int(cmdlinearg('seed', sys.argv[-1])))
 
 
 n = int(cmdlinearg('n'))
-print(n)
-a = [random.randint(-5, 5) for i in range(n)]
-print(' '.join(map(str, a)))
-b = [random.randint(2, n)]
-for i in range(1, n): b.append(random.randint(1, i))
-print(' '.join(map(str, b)))
+m = random.randint(1, n * (n - 1) // 2)
+
+connections = [set() for _ in range(n)]
+for _ in range(m):
+    a, b = -1, -1
+    while a == b or b in connections[a] or a in connections[b]:
+        a, b = random.randrange(0, n), random.randrange(0, n)
+    connections[a].add(b)
+    connections[b].add(a)
+
+def dfs(connections, visited, at):
+    res = set([at])
+    if at in visited:
+        return res
+    visited.add(at)
+
+    for b in connections[at]:
+        res.update(dfs(connections, visited, b))
+
+    return res
+
+not_reached = set(range(n))
+while len(not_reached) > 0:
+    start = list(not_reached)[0]
+    reachable = dfs(connections, set(), start)
+    not_reached = not_reached.difference(reachable) # will remove start
+
+    if len(reachable) > 2:
+        start = random.choice(list(reachable))
+        reachable.remove(start)
+        end = random.choice(list(reachable))
+        break
+
+print(n, m)
+print(start, end)
+print(" ".join(str(random.randrange(-1e1, 1e1)) for _ in range(n)))
+
+connections = set(
+    (min(a, b), max(a, b))
+    for a in range(n)
+    for b in connections[a]
+)
+
+connections = list(connections)
+random.shuffle(connections)
+
+for a, b in connections:
+    if random.random() < 0.5:
+        print(a + 1, b + 1)
+    else:
+        print(b + 1, a + 1)
