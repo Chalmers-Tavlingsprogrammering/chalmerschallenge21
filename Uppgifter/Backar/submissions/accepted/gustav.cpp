72ac00277caba9c24b688fc8cc809b8432341e49
Gustav <gustav.kalander@hotmail.se>
2021-01-21 00:55:12 +0100

    [backar] added sol, improved n2.cpp

--- Uppgifter/Backar/submissions/accepted/gustav.cpp
+++ Uppgifter/Backar/submissions/accepted/gustav.cpp
@@ -10,13 +10,11 @@ typedef vector<ll> vl;
 #define all(x) (x).begin(), (x).end()
 #define sz(x) ((int)(x).size())
 constexpr int inf = 0x3f3f3f3f;
-constexpr ll linf = 1234567890123456789;
 
 int n, m;
 int from, to;
 int heights[100000];
 int visited[100000];
-ll kdist[100000];
 vvi adj;
 
 int main(){
@@ -32,18 +30,14 @@ int main(){
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    for(int i = 0; i < n; i++) kdist[i] = linf;
-    vector<pair<ll, int>> pq; //långsam priority_queue
-    pq.emplace_back(0, from);
+    priority_queue<pair<ll, int>> pq;
+    pq.emplace(0, from);
     while(1){
-        tuple<ll, int, int> b = {linf, 0, 0};
-        for(int i = 0; i < sz(pq); i++){
-            b = min(b, {pq[i].first, pq[i].second, i});
-        }
         ll dist;
-        int pos, ind;
-        tie(dist, pos, ind) = b;
-        pq.erase(pq.begin() + ind);
+        int pos;
+        tie(dist, pos) = pq.top();
+        dist = -dist;
+        pq.pop();
         if(visited[pos]) continue;
         visited[pos] = 1;
         if(pos == to){
@@ -51,13 +45,7 @@ int main(){
             return 0;
         }
         for(int i = 0; i < sz(adj[pos]); i++){
-            if(!visited[adj[pos][i]]){
-                ll ndist = dist+max(0, heights[adj[pos][i]] - heights[pos]);
-                if(ndist < kdist[adj[pos][i]]){
-                    kdist[adj[pos][i]] = ndist;
-                    pq.emplace_back(ndist, adj[pos][i]);
-                }
-            }
+            pq.emplace(-dist-max(0, heights[adj[pos][i]] - heights[pos]), adj[pos][i]);
         }
     }
 }
